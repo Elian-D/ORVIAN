79430e75112a4faab5410a5262eb55c642ca9b5e
Elian-D <eliandavidbertrecuello19@gmail.com>
2026-03-06 14:57:29 -0400

    feat(geo): agregar jerarquía territorial de República Dominicana

---
+++ database/migrations/2026_03_06_021816_create_sections_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            // Hacemos el distrito nullable
+            $table->foreignId('district_id')->nullable()->constrained()->onDelete('cascade');
+            // Añadimos municipio_id para las secciones que son "Zona Urbana" directa del municipio
+            $table->foreignId('municipality_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
