c39990f8066d120bb41a557899c346dbdc7f2197
Elian-D <eliandavidbertrecuello19@gmail.com>
2026-03-28 20:54:26 -0400

    feat(roles): agregar rutas para gestion de roles en admin y tenant

---
+++ routes/app/role.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\App\Roles\RoleIndex;
+use App\Livewire\Shared\Roles\RoleForm;
+
+Route::middleware(['can:roles.view'])->prefix('roles')->name('roles.')->group(function () {
+    Route::get('/', RoleIndex::class)->name('index');
+    
+    Route::middleware(['can:roles.create'])->group(function () {
+        Route::get('/create', RoleForm::class)->name('create');
+        Route::get('/{role}/edit', RoleForm::class)->name('edit');
+        Route::get('/{role}/permissions', \App\Livewire\Shared\Roles\RolePermissions::class)->name('permissions');
+    });
+    // Matriz de permisos se agrega en Fase 5.4
+});
